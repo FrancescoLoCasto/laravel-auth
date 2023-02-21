5697ef793b3da80c6070beeef5592c5a26536124
MyUsername <my@email.com>
2023-02-21 21:33:15 +0100

    index creation on post controller

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -101,11 +101,12 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Post $post
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('admin.posts.index') ->with('message', "The post $post->title has been deleted")->with('message-class','warning');
     }
 }
