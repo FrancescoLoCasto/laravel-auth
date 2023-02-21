38fa87e4b5bd23b5771327046a2806f5dc1d0159
MyUsername <my@email.com>
2023-02-21 01:41:48 +0100

    seeder litle change for faker

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -20,12 +20,11 @@ class PostSeeder extends Seeder
         //
         for ($i=0; $i <50 ; $i++) { 
             $newPost = new Post();
-            $newPost->title = $faker->unique()->sentence(4);
+            $newPost->title = $faker->unique()->realTextBetween(5, 20);
             $newPost->slug = Str::slug($newPost->title);
             $newPost->author = $faker->name();
-            $newPost->content = $faker->text(200);
+            $newPost->content = $faker->realTextBetween(600, 800);
             $newPost->post_date = $faker->dateTimeThisYear();
-            $newPost->content = $faker->text(200);
             $newPost->save();
         }
     }
