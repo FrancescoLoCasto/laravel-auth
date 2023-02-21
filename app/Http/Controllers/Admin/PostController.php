4ac519718d70d8c75a7279278284d0169305a8e3
MyUsername <my@email.com>
2023-02-21 01:00:13 +0100

    creation table element on post table php

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PostController.php extends Controller
+class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
