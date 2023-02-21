08cee5f3e6ea38ee8e2f6c1fbd04be413248dafe
MyUsername <my@email.com>
2023-02-21 02:42:51 +0100

    create all table

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,6 +2,38 @@
 
 @section('content')
 <div class="container">
+   <table class="table table-striped table-bordered table-hover m-5">
+      <thead>
+        <tr>
+          <th scope="col">#id</th>
+          <th scope="col">Title</th>
+          <th scope="col">Author</th>
+          <th scope="col">Post</th>
+          <th scope="col">Actions</th>
 
+        </tr>
+      </thead>
+      <tbody>
+         @foreach ($posts as $post)
+         <tr>
+            <td>{{$post->id}}</td>
+            <td>{{$post->title}}</td>
+            <td>{{$post->author}}</td>
+            <td>{{$post->post_date}}</td>
+            <td>
+               <a href="" class="btn btn-sm btn-primary">
+                  Edit
+               </a>
+               <a href="" class="btn btn-sm btn-warning">
+                  Show
+               </a>
+               <a href="" class="btn btn-sm btn-danger">
+                  Delete
+               </a>
+            </td>
+         </tr>
+         @endforeach
+      </tbody>
+    </table>
 </div>
 @endsection
