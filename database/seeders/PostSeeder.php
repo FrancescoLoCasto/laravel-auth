111b78366ebc0d82c47a0c4d2f0570861fc535f4
MyUsername <my@email.com>
2023-02-21 01:19:54 +0100

    creation change slug migration

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,16 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
+        for ($i=0; $i <50 ; $i++) { 
+            $newPost = new Post();
+            $newPost->slug = '';
+            $newPost->title = '';
+            $newPost->author = '';
+            $newPost->content = '';
+            $newPost->date = '';
+        }
     }
 }
